8ccca2556f5384047d47359868a4bb79151416e8
Tosin <aoluwatosin10@gmail.com>
2021-12-26 02:56:08 +0000

    update

--- test.py
+++ test.py
@@ -11,20 +11,81 @@ from pybatfish.client.asserts import (
     assert_no_unestablished_bgp_sessions,
     assert_no_undefined_references,
 )
-from rich import print as rprint
+from rich.console import Console
+
+
+console = Console(color_system="truecolor")
 
 
 def test_duplicate_rtr_ids(snap):
     """Testing for duplicate router IDs"""
+    console.print(
+        ":white_exclamation_mark: [bold yellow]Testing for duplicate router IDs[/bold yellow] :white_exclamation_mark:"
+    )
     assert_no_duplicate_router_ids(
         snapshot=snap,
         protocols={"ospf", "bgp"},
+    )
+    console.print(
+        ":green_heart: [bold green]No duplicate router IDs found[/bold green] :green_heart:"
+    )
+
+
+def test_bgp_compatibility(snap):
+    """Testing for incompatible BGP sessions"""
+    console.print(
+        ":white_exclamation_mark: [bold yellow]Testing for incompatible BGP sessions[/bold yellow] :white_exclamation_mark:"
+    )
+    assert_no_incompatible_bgp_sessions(
+        snapshot=snap,
+    )
+    console.print(
+        ":green_heart: [bold green]All BGP sessions compatible![/bold green] :green_heart:"
+    )
+
+
+def test_ospf_compatibility(snap):
+    """Testing for incompatible OSPF sessions"""
+    console.print(
+        ":white_exclamation_mark: [bold yellow]Testing for incompatible OSPF sessions[/bold yellow] :white_exclamation_mark:"
+    )
+    assert_no_incompatible_ospf_sessions(
+        snapshot=snap,
+    )
+    console.print(
+        ":green_heart: [bold green]All OSPF sessions compatible![/bold green] :green_heart:"
+    )
+
+
+def test_bgp_unestablished(snap):
+    """Testing for BGP sessions that are not established"""
+    console.print(
+        ":white_exclamation_mark: [bold yellow]Testing for unestablished BGP sessions[/bold yellow] :white_exclamation_mark:"
+    )
+    assert_no_unestablished_bgp_sessions(
+        snapshot=snap,
+    )
+    console.print(
+        ":green_heart: [bold green]All BGP sessions are established![/bold green] :green_heart:"
+    )
+
+
+def test_undefined_references(snap):
+    """Testing for any undefined references"""
+    console.print(
+        ":white_exclamation_mark: [bold yellow]Testing for undefined references[/bold yellow] :white_exclamation_mark:"
+    )
+    assert_no_undefined_references(
+        snapshot=snap,
+    )
+    console.print(
+        ":green_heart: [bold green]No undefined refences found![/bold green] :green_heart:"
     )
 
 
 def main():
     """init all the things"""
-    NETWORK_NAME = "EOS_NET"
+    NETWORK_NAME = "PDX_NET"
     SNAPSHOT_NAME = "snapshot00"
     SNAPSHOT_DIR = "./snapshots"
     bf_session.host = "192.168.7.45"
