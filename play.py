c22ba1ae11b356e9dfc676be9fac34554d4dc120
Aramide <aoluwatosin10@gmail.com>
2021-12-27 04:36:15 +0000

    Update

--- play.py
+++ play.py
@@ -10,15 +10,14 @@ from nornir_napalm.plugins.tasks import napalm_get
 from yaml import safe_load
 import urllib3
 from tools import nornir_set_creds
-
 # Disable warnings
 # If running into ArubaCX sessions full error or blank info
 # run the following at the console "https-server session close all"
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
 norn = InitNornir(config_file="configs/config.yaml", core={"raise_on_error": True})
 nornir_set_creds(norn)
 app = FastAPI()
+
 
 
 @app.get("/")
@@ -27,12 +26,14 @@ async def root():
     return {"message": "Hello JulioPDX"}
 
 
+
 @app.get("/devices")
 async def get_devices():
     """Returns a list of devices loaded from our hosts file"""
     with open("./configs/hosts.yaml", encoding="utf-8") as file:
         devices = safe_load(file)
     return {"devices": devices}
+
 
 
 @app.get("/devices/{hostname}/napalm_get/{getter}")
